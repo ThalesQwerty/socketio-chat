b005eb805097d92a99fefb67381f75fc9cc85c8e
Thales Rodrigues <thales.brito@hotmail.com>
2020-08-22 01:42:36 -0300

    Fixed a bug where user avatars wouldn't show properly

--- src/components/user/UserImage.jsx
+++ src/components/user/UserImage.jsx
@@ -3,20 +3,24 @@ import React from "react";
 import STYLE from "./styles/user.module.scss";
 
 import {
+    Box
+} from "@material-ui/core";
+
+import {
     Person as PersonIcon, Style
 } from "@material-ui/icons"
 
 const defaultImage = (props) => (
-    <div 
+    <Box
         className={STYLE.default_image_container} 
         style={{
             marginRight: props.marginRight || '1rem',
             marginLeft: props.marginLeft || '0rem',
-            color: props.user.color || 'gray'
+            backgroundColor: props.user.color || 'gray'
         }}
     >
         <PersonIcon className={STYLE.default_image} />
-    </div>
+    </Box>
 );
 
 export default (props) =>
