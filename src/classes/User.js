f74a27bac2179c22edb2aad2cb0ce21788b69487
Thales Rodrigues <thales.brito@hotmail.com>
2020-09-01 23:58:51 -0300

    User class on front-end is now static

--- src/classes/User.js
+++ src/classes/User.js
@@ -1,27 +1,6 @@
 class User {
-    constructor(name, color) {
-        this.id = User.list.length + 1;
-        this.name = name;
-        this.color = color;
-
-        User.list.push(this);
-    }
-
     static list = [];
-
-    static random() {
-        do {
-            var selected = User.list[Math.floor(Math.random() * User.list.length)];
-        } while (
-            User.same([selected, User.me()])
-        );
-        
-        return selected;
-    }
-
-    static me() {
-        return User.list[0];
-    }
+    static me = null;
 
     static same(users) {
         for (let i = 1; i < users.length; i++) {
